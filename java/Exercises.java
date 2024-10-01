51f1f1fad9514a11052c6888e034c6020bac2809
giselleeliasi <112521190+giselleeliasi@users.noreply.github.com>
2024-09-30 17:01:02 -0700

    record class 3 failing

--- java/Exercises.java
+++ java/Exercises.java
@@ -29,7 +29,7 @@ public class Exercises {
                 .findFirst();                    
     }
 
-    // Write your say function here
+
     public static SayChainable say() {
         return new SayChainable() {
             @Override
@@ -64,7 +64,7 @@ public class Exercises {
     }
 
 
-    // Write your line count function here
+
     public static long meaningfulLineCount(String filename) throws IOException {
         long count = 0;
         
@@ -81,7 +81,75 @@ public class Exercises {
     }
     
 }
-// Write your Quaternion record class here
 
+
+record Quaternion(double a, double b, double c, double d) {
+    public static final Quaternion ZERO = new Quaternion(0.0, 0.0, 0.0, 0.0);
+    public static final Quaternion I = new Quaternion(0.0, 1.0, 0.0, 0.0);
+    public static final Quaternion J = new Quaternion(0.0, 0.0, 1.0, 0.0);
+    public static final Quaternion K = new Quaternion(0.0, 0.0, 0.0, 1.0);
+
+    public Quaternion {
+        if (Double.isNaN(a) || Double.isNaN(b) || Double.isNaN(c) || Double.isNaN(d)) {
+            throw new IllegalArgumentException("Coefficients cannot be NaN");
+        }
+    }
+
+    public Quaternion plus(Quaternion other) {
+        return new Quaternion(
+                this.a + other.a,
+                this.b + other.b,
+                this.c + other.c,
+                this.d + other.d
+        );
+    }
+
+    public Quaternion times(Quaternion other) {
+        return new Quaternion(
+                this.a * other.a - this.b * other.b - this.c * other.c - this.d * other.d,
+                this.a * other.b + this.b * other.a + this.c * other.d - this.d * other.c,
+                this.a * other.c - this.b * other.d + this.c * other.a + this.d * other.b,
+                this.a * other.d + this.b * other.c - this.c * other.b + this.d * other.a
+        );
+    }
+
+    public Quaternion conjugate() {
+        return new Quaternion(this.a, -this.b, -this.c, -this.d);
+    }
+
+    public double[] coefficients() {
+        return new double[] {this.a, this.b, this.c, this.d};
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        if (a != 0.0) sb.append(a);
+
+        if (b != 0.0) {
+            if (b > 0 && sb.length() > 0) sb.append("+");
+            if (b == -1.0) sb.append("-");
+            else if (b != 1.0) sb.append(b);
+            sb.append("i");
+        }
+
+        if (c != 0.0) {
+            if (c > 0 && sb.length() > 0) sb.append("+");
+            if (c == -1.0) sb.append("-");
+            else if (c != 1.0) sb.append(c);
+            sb.append("j");
+        }
+
+        if (d != 0.0) {
+            if (d > 0 && sb.length() > 0) sb.append("+");
+            if (d == -1.0) sb.append("-");
+            else if (d != 1.0) sb.append(d);
+            sb.append("k");
+        }
+
+        if (sb.length() == 0) sb.append("0");
+        return sb.toString();
+    }
+}
 
 // Write your BinarySearchTree sealed interface and its implementations here
